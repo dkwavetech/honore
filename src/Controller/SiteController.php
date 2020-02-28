1491edd62f68bc1096f80909f3488749a05f6493
danielle kayumbi <danielle.kayumbi@manomano.com>
2020-02-28 04:10:12 +0100

    fix

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SiteController extends AbstractController
+{
+    /**
+      * @Route("/in-loving-memory")
+      */
+    public function index()
+    {
+        return $this->render('site/index.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/in-loving-memory/programme")
+     */
+    public function programme()
+    {
+        return $this->render('site/programme.html.twig', [
+        ]);
+    }
+}
